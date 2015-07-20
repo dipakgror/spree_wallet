9f8e27ee87b86913a9e660e908e1913925798007
dipakgror <dipakg.ror@gmail.com>
2015-07-20 14:07:45 +0530

    Update set balance method for effective amount

--- lib/spree/disable_negative_payment_mode_and_set_balance_ability.rb
+++ lib/spree/disable_negative_payment_mode_and_set_balance_ability.rb
@@ -12,7 +12,7 @@ module Spree
     end
 
     def set_balance
-      self.balance = user.store_credits_total + effective_amount if user && amount
+      self.balance = user.store_credits_total + (effective_amount(amount) || 0.00) if user && amount
     end
   end
 end
